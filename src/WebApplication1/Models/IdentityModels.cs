5dd8f9a78314a7acfd9c7347ec37671ed9d335a0
dagolobrea <dagolobrea@gmail.com>
2014-11-14 20:55:30 +0100

    Change in identitymodel

--- src/WebApplication1/Models/IdentityModels.cs
+++ src/WebApplication1/Models/IdentityModels.cs
@@ -19,7 +19,7 @@ namespace WebApplication1.Models
         
         public ApplicationDbContext()
         {            
-            // Create the database and schema if it doesn't exist
+            // //Create the database and schema if it doesn't exist
             // This is a temporary workaround to create database until Entity Framework database migrations 
             // are supported in ASP.NET 5
             if (!_created)
